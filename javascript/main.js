635b6bf2986212825163cbdc8b197ae03ed3f690
hamid ebadi <hamidebadi1996@yahoo.com>
2020-12-24 21:07:32 +0200

    database design and testing

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,8 @@ let xHttp = new XMLHttpRequest();
  //Action when app recieves the answer
  xHttp.onreadystatechange = function (){
  	if(this.readyState == 4 && this.status == 200){
- 		console.log(this.responsText);
+ 		var dataObj = JSON.parse(this.responseText);
+ 		console.log(dataObj[0].title);
  	}
  }
 
